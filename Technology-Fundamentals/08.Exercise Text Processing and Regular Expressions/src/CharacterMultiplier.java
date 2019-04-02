3b70eb0176970b5c48abd21777d40eb49051dc92
asennesa <angeorg117@gmail.com>
2019-04-02 16:06:15 +0300

    Second exercise.

--- Technology-Fundamentals/08.Exercise Text Processing and Regular Expressions/src/CharacterMultiplier.java
+++ Technology-Fundamentals/08.Exercise Text Processing and Regular Expressions/src/CharacterMultiplier.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class CharacterMultiplier1 {
+public class CharacterMultiplier {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         String text = reader.readLine();
