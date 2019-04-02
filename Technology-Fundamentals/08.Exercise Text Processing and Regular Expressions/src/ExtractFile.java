9c985ead0f2403154230ea1a119cb5393b377b32
asennesa <angeorg117@gmail.com>
2019-04-02 15:58:20 +0300

    Third exercise

---
+++ Technology-Fundamentals/08.Exercise Text Processing and Regular Expressions/src/ExtractFile.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ExtractFile {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String text = reader.readLine();
+
+        String firstWord = text.substring(text.lastIndexOf('\\')+1,text.lastIndexOf('.'));
+        String secondWord = text.substring(text.lastIndexOf('.')+1);
+        System.out.println(String.format("File name: %s",firstWord));
+        System.out.println(String.format("File extension: %s",secondWord));
+
+
+
+    }
+}
+
