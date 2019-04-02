52af04979644b429064efa4c6e1780aefe5d8e38
asennesa <angeorg117@gmail.com>
2019-04-02 15:42:40 +0300

    First exercise.

--- Technology-Fundamentals/08.Exercise Text Processing and Regular Expressions/src/ValidUsername.java
+++ Technology-Fundamentals/08.Exercise Text Processing and Regular Expressions/src/ValidUsername.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class validUsername {
+public class ValidUsername {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
