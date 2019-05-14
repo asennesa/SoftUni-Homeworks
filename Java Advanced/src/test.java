f5296f8dc5e9545dbe79cbe5c9ae3d91fbc8af5b
asennesa <angeorg117@gmail.com>
2019-05-14 14:11:37 +0300

    Test

---
+++ Java Advanced/src/test.java
@@ -0,0 +1,11 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class test {
+    public static void main(String[] args){
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("test");
+
+    }
+}
