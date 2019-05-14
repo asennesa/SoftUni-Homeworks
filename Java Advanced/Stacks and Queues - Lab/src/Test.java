74599f1104b5f1112874900f8d394693992098e3
asennesa <angeorg117@gmail.com>
2019-05-14 14:20:38 +0300

    Test

--- Java Advanced/Stacks and Queues - Lab/src/Test.java
+++ Java Advanced/Stacks and Queues - Lab/src/Test.java
@@ -1,11 +1,9 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-public class test {
-    public static void main(String[] args){
+public class Test {
+    public static void main(String[] args) {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-
-        System.out.println("test");
-
+        System.out.println("Hey");
     }
 }
