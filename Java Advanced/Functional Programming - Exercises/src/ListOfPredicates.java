96a56f50bb6b46f56ef3330dc020866c9f9bcc0a
asennesa <angeorg117@gmail.com>
2019-06-04 16:58:06 +0300

    Done

--- Java Advanced/Functional Programming - Exercises/src/ListOfPredicates.java
+++ Java Advanced/Functional Programming - Exercises/src/ListOfPredicates.java
@@ -5,7 +5,6 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Scanner;
 import java.util.function.BiPredicate;
 import java.util.stream.Collectors;
 
